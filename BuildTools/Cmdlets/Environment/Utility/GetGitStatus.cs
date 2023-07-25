1f280bb0e77cd86e829de61b89295f6a198f5b72
lordmilko <lordmilko@users.noreply.github.com>
2023-07-25 19:15:44 +1000

    Implement GetGitStatus

--- BuildTools/Cmdlets/Environment/Utility/GetGitStatus.cs
+++ BuildTools/Cmdlets/Environment/Utility/GetGitStatus.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Management.Automation;
 
 namespace BuildTools.Cmdlets
 {
+    class GitFileStatus
+    {
+        public string Path { get; set; }
+        public GitChangeType? StagedStatus { get; set; }
+        public GitChangeType? UnstagedStatus { get; set; }
+    }
+
+    enum GitChangeType
+    {
+        Added,
+        Modified,
+        Renamed,
+        Deleted
+    }
+
     [Cmdlet(VerbsCommon.Get, "GitStatus")]
     [BuildCommand(CommandKind.GitStatus, CommandCategory.Utility)]
     public abstract class GetGitStatus<TEnvironment> : BuildCmdlet<TEnvironment>
@@ -22,7 +38,77 @@ Files with changes are flagged as having either unstaged or staged changes. File
 
         protected override void ProcessRecordEx()
         {
-            throw new NotImplementedException();
+            var configProvider = GetService<IProjectConfigProvider>();
+            var process = GetService<IProcessService>();
+
+            var lines = process.Execute(
+                "git",
+                new ArgList
+                {
+                    "-C",
+                    configProvider.SolutionRoot,
+                    "status",
+                    "--porcelain"
+                }
+            );
+
+            var results = new List<GitFileStatus>();
+
+            foreach (var line in lines)
+            {
+                var code = line.Substring(0, 2);
+                var path = line.Substring(3);
+
+                var status = new GitFileStatus
+                {
+                    Path = path
+                };
+
+                switch (code)
+                {
+                    case " M":
+                        //The file has been modified and no part of it has been staged
+                        status.UnstagedStatus = GitChangeType.Modified;
+                        break;
+
+                    case "M ":
+                        //The file has been modified and all parts of it have been staged
+                        status.StagedStatus = GitChangeType.Modified;
+                        break;
+
+                    case "MM":
+                        status.StagedStatus = GitChangeType.Modified;
+                        status.UnstagedStatus = GitChangeType.Modified;
+                        break;
+
+                    case "A ":
+                        //A new file has been added and has been staged
+                        status.StagedStatus = GitChangeType.Added;
+                        break;
+
+                    case "R ":
+                        //A rename of a file has been staged
+                        status.StagedStatus = GitChangeType.Renamed;
+                        break;
+
+                    case "D ":
+                        //A deletion of a file has been staged
+                        status.StagedStatus = GitChangeType.Deleted;
+                        break;
+
+                    case "??":
+                        status.UnstagedStatus = GitChangeType.Added;
+                        break;
+
+                    default:
+                        throw new NotImplementedException($"Don't know how to handle code '{code}' for item '{line}'");
+                }
+
+                results.Add(status);
+            }
+
+            foreach (var result in results)
+                WriteObject(result);
         }
     }
 }
