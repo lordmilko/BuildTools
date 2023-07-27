3ca16b07c19fb5f33b30b689fc4a2e36380712c1
lordmilko <lordmilko@users.noreply.github.com>
2023-07-27 19:22:04 +1000

    Implement GetHelp

--- BuildTools/Cmdlets/Environment/Help/GetHelp.cs
+++ BuildTools/Cmdlets/Environment/Help/GetHelp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Management.Automation;
+using BuildTools.PowerShell;
 
 namespace BuildTools.Cmdlets
 {
@@ -9,21 +11,28 @@ namespace BuildTools.Cmdlets
     {
         public static void CreateHelp(HelpConfig help, ProjectConfig project)
         {
-            help.Synopsis = $"Opens the {project.Name} Wiki for getting help with the {project.Name} Build Environment.";
+            help.Synopsis = $"Opens the BuildTools Wiki for getting help with the {project.Name} Build Environment.";
             help.Description = $@"
-The {help.Command} cmdlet opens the {project.Name} Wiki page containing detailed instructions on compiling {project.Name} and using the {project.Name} Build Environment.
+The {help.Command} cmdlet opens the BuildTools Wiki page containing detailed instructions on compiling {project.Name} and using the {project.Name} Build Environment.
 
 Note: due to limitations of the Unix platform, when running on Linux or macOS the {help.Command} cmdlet will instead display the URL that you should navigate to instead of automatically opening the URL in your default web browser.";
 
             help.Examples = new[]
             {
-                new HelpExample(help.Command, $"Open the {project.Name} Wiki article detailing how to compile {project.Name}.")
+                new HelpExample(help.Command, $"Open the BuildTools Wiki article detailing how to compile {project.Name}.")
             };
         }
 
         protected override void ProcessRecordEx()
         {
-            throw new NotImplementedException();
+            var powershell = GetService<IPowerShellService>();
+
+            var url = "https://github.com/lordmilko/BuildTools/wiki/Build-Environment";
+
+            if (powershell.IsWindows)
+                Process.Start(url);
+            else
+                Console.WriteLine($"BuildTools Wiki: {url}");
         }
     }
 }
